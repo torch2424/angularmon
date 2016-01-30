2d2768846d43c984b796f9bd9af1ddf93831882b
Aaron <torch2424@hotmail.com>
2016-01-29 17:53:30 -0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularmonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularmonApp
+ */
+angular.module('angularmonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
