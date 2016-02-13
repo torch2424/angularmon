2a49d09141eda277ee0c98e9736f7debe31de67c
Aaron Turner <aaron@aaronthedev.com>
2016-02-13 14:12:40 -0800

    Working on functions and promises
    Had to leave however, this is almost done

--- backend/routes/angularmon.js
+++ backend/routes/angularmon.js
@@ -40,119 +40,89 @@ router.get('/', function(req, res, next) {
     //max num pokemon sprites (719)
     var pokemonId = Math.floor((Math.random() * 719) + 1) + ',' +  Math.floor((Math.random() * 719) + 1);
 
-  api.get('pokemon', pokemonId).then(function(resPokemon) {
+    //Get our pokemon
+    var resPokemon = getPokemon(pokemonId);
+
+    console.log(resPokemon);
+
+    resPokemon.then(function() {
+
+        console.log(resPokemon);
 
         //Next get our sprites, build an array
-        var reqSprites = [resPokemon[0].sprites[0], resPokemon[1].sprites[0]];
+        var reqSprites = [resPokemon.value[0].sprites[0], resPokemon.value[1].sprites[0]];
 
-        //Now request
-    	api.get(reqSprites).then(function(resSprites) {
+        //Get our sprites
+        var resSprites = getSprite(reqSprites);
 
-            api.get(resPokemon[0].moves).then(function(resMovesOne) {
+        //Get Our moves
+        var resMoves = [];
+        resMoves[0] = getMoves(resPokemon.value[0].moves);
+        resMoves[1] = getMoves(resPokemon.value[1].moves);
 
-                if(!resMovesOne ||
-                resMovesOne.length < 4) {
-                    res.status(500).json({
-                          msg: "Could not ping the pokeapi for moves!"
-                    });
+        //Save our stats while we are waiting
+        pokemon[0].name = resPokemon.value[0].name;
+        pokemon[1].name = resPokemon.value[1].name;
+        pokemon[0].hp = resPokemon.value[0].hp;
+        pokemon[1].hp = resPokemon.value[1].hp;
+        pokemon[0].attack = resPokemon.value[0].attack;
+        pokemon[1].attack = resPokemon.value[1].attack;
+        pokemon[0].defense = resPokemon.value[0].defense;
+        pokemon[1].defense = resPokemon.value[1].defense;
+        pokemon[0].sp_atk = resPokemon.value[0].sp_atk;
+        pokemon[1].sp_atk = resPokemon.value[1].sp_atk;
+        pokemon[0].sp_def = resPokemon.value[0].sp_def;
+        pokemon[1].sp_def = resPokemon.value[1].sp_def;
+        pokemon[0].speed = resPokemon.value[0].speed;
+        pokemon[1].speed = resPokemon.value[1].speed;
+
+        //Call the rest of requried things
+        Promise.all([resSprites, resMoves[0], resMoves[1]]).then(function() {
+
+            //Finish up and return! :D
+
+            //Now that we have EVERYTHING, start building our response
+
+            //Save our image urls
+            pokemon[0].spriteUrl = "http://pokeapi.co" + resSprites.value[0].image
+            pokemon[1].spriteUrl = "http://pokeapi.co" + resSprites.value[1].image
+
+            //Finally loop through and grab moves
+            var pokeMoves = [resMoves[0], resMoves[1]];
+
+            for(var i = 0; i < pokemon.length; i++) {
+
+                //An array of already added moves
+                var addedIndex = [];
+
+                //And we need four moves
+                while(pokemon[i].moves.length < 4) {
+
+                    //Get a random move
+                    var ranMove = Math.floor(Math.random() * pokeMoves[i].length);
+
+                    //Check if it was already added
+                    var added = false;
+                    for(var k = 0; k < addedIndex.length; k++) {
+                        if(ranMove == addedIndex[k]) added = true;
+                    }
+
+                    //find moves with power greater than one
+                    if(!added &&
+                        pokeMoves[i][ranMove].power > 1) {
+
+                        //Add it to the pokemon
+                        pokemon[i].moves.push(pokeMoves[i][ranMove]);
+
+                        //Also, Add it to the added index
+                        addedIndex.push(ranMove);
+                    }
                 }
-
-                api.get(resPokemon[1].moves).then(function(resMovesTwo) {
-
-                    if(!resMovesTwo ||
-                    resMovesTwo.length < 4) {
-                        res.status(500).json({
-                              msg: "Could not ping the pokeapi for moves!"
-                        });
-                    }
-
-                    //Now that we have EVERYTHING, start building our response
-
-                    //Save Our Name
-                    pokemon[0].name = resPokemon[0].name;
-                    pokemon[1].name = resPokemon[1].name;
-
-                    //Save our stats
-                    pokemon[0].hp = resPokemon[0].hp;
-                    pokemon[1].hp = resPokemon[1].hp;
-                    pokemon[0].attack = resPokemon[0].attack;
-                    pokemon[1].attack = resPokemon[1].attack;
-                    pokemon[0].defense = resPokemon[0].defense;
-                    pokemon[1].defense = resPokemon[1].defense;
-                    pokemon[0].sp_atk = resPokemon[0].sp_atk;
-                    pokemon[1].sp_atk = resPokemon[1].sp_atk;
-                    pokemon[0].sp_def = resPokemon[0].sp_def;
-                    pokemon[1].sp_def = resPokemon[1].sp_def;
-                    pokemon[0].speed = resPokemon[0].speed;
-                    pokemon[1].speed = resPokemon[1].speed;
-
-                    //Save our image urls
-                    pokemon[0].spriteUrl = "http://pokeapi.co" + resSprites[0].image
-                    pokemon[1].spriteUrl = "http://pokeapi.co" + resSprites[1].image
-
-                    //Finally loop through and grab moves
-                    var pokeMoves = [resMovesOne, resMovesTwo];
-
-                    for(var i = 0; i < pokemon.length; i++) {
-
-                        //An array of already added moves
-                        var addedIndex = [];
-
-                        //And we need four moves
-                        while(pokemon[i].moves.length < 4) {
-
-                            //Get a random move
-                            var ranMove = Math.floor(Math.random() * pokeMoves[i].length);
-
-                            //Check if it was already added
-                            var added = false;
-                            for(var k = 0; k < addedIndex.length; k++) {
-                                if(ranMove == addedIndex[k]) added = true;
-                            }
-
-                            //find moves with power greater than one
-                            if(!added &&
-                                pokeMoves[i][ranMove].power > 1) {
-
-                                //Add it to the pokemon
-                                pokemon[i].moves.push(pokeMoves[i][ranMove]);
-
-                                //Also, Add it to the added index
-                                addedIndex.push(ranMove);
-                            }
-                        }
-                    }
+            }
 
 
-                    //FINALLY RETURN OUR POKEMON!
-                    res.send(pokemon);
-
-                },
-                //Error
-                function(err) {
-                    res.status(500).json({
-                          msg: "Could not ping the pokeapi for moves!"
-                    });
-                })
-            },
-            //Error
-            function(err) {
-                res.status(500).json({
-                      msg: "Could not ping the pokeapi for moves!"
-                });
-            })
-        },
-        //Error
-        function(err) {
-            res.status(500).json({
-                  msg: "Could not ping the pokeapi for sprites!"
-            });
-        })
-    },
-    //Error, give status to the frontend
-    function(err) {
-        res.status(500).json({
-              msg: "Could not ping the pokeapi for sprite!"
+            //FINALLY RETURN OUR POKEMON!
+            res.send(pokemon);
         });
     });
 });
@@ -160,22 +130,81 @@ router.get('/', function(req, res, next) {
 
 
 //Function to grab a pokemon
-var getPokemon = function() {
-      api.get('pokemon', pokemonId).then(function(resPokemon) {
+var getPokemon = function(pokemonId) {
 
-          //Return the response
-          return resPokemon;
-      },
-      //Error, give status to the frontend
-      function(err) {
-          res.status(500).json({
-                msg: "Could not ping the pokeapi for sprite!"
-          });
+    //Request from the api
+      return api.get('pokemon', pokemonId);
 
-          //Return false
-          return false;
-      });
+    //   .then(function(resPokemon) {
+    //
+    //       //Success
+    //       //Return the response
+    //       return resPokemon;
+    //   },
+    //   //Error, give status to the frontend
+    //   function(err) {
+    //       res.status(500).json({
+    //             msg: "Could not ping the pokeapi for the pokemon!!"
+    //       });
+    //
+    //       //Return false
+    //       return false;
+    //   });
 
+}
+
+
+//Function to grab a sprite
+//Pass an array to grab multipl sprites
+var getSprite = function(reqSprites) {
+
+    //Request from the api
+    return api.get(reqSprites);
+
+    // .then(function(resSprites) {
+    //
+    //     //Success
+    //     return resSprites;
+    // },
+    // //Error
+    // function(err) {
+    //     res.status(500).json({
+    //           msg: "Could not ping the pokeapi for sprites!"
+    //     });
+    //
+    //     return false;
+    // })
+}
+
+//Function to grab an array of moves
+var getMoves = function(reqMoves) {
+
+    //Request from the api
+    //Was resPokemon[0].moves
+    return api.get(reqMoves);
+
+    // .then(function(resMoves) {
+    //
+    //     //Success
+    //     return resMoves;
+    // },
+    //
+    // //Error
+    // function(err) {
+    //     res.status(500).json({
+    //           msg: "Could not ping the pokeapi for moves!"
+    //     });
+    //
+    //     //Return false
+    //     return false;
+    // })
+
+    // if(!resMovesOne ||
+    // resMovesOne.length < 4) {
+    //     res.status(500).json({
+    //           msg: "Could not ping the pokeapi for moves!"
+    //     });
+    // }
 }
 
 
