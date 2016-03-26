365a5339a39b3867b723bf6e5e05a13c45059826
Aaron Turner <aaron@aaronthedev.com>
2016-03-26 13:43:59 -0700

    Fixed up the navbar for mobile, and added an about page

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularmonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularmonApp
+ */
+angular.module('angularmonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
